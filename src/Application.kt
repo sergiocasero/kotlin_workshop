558254282e2a30bf6ac8447fa140065c2967fd86
Sergio Casero hernández <sergio.casero23@gmail.com>
2019-03-13 09:01:31 +0100

    deleted unused imports

--- src/Application.kt
+++ src/Application.kt
@@ -1,23 +1,12 @@
 package com.worldline.kotlin
 
 import io.ktor.application.Application
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.auth.Authentication
 import io.ktor.auth.UserIdPrincipal
 import io.ktor.auth.basic
-import io.ktor.auth.principal
-import io.ktor.html.respondHtml
-import io.ktor.http.ContentType
-import io.ktor.http.content.resources
-import io.ktor.http.content.static
-import io.ktor.response.respondText
-import io.ktor.routing.get
+import io.ktor.routing.route
 import io.ktor.routing.routing
-import kotlinx.html.body
-import kotlinx.html.h1
-import kotlinx.html.li
-import kotlinx.html.ul
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -31,35 +20,13 @@ fun Application.module(testing: Boolean = false) {
         }
     }
 
+    geopoint()
+}
+
+fun Application.geopoint() {
     routing {
-        get("/") { call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain) }
+        route("/geopoint") {
 
-        get("/html-dsl") {
-            call.respondHtml {
-                body {
-                    h1 { +"HTML" }
-                    ul {
-                        for (n in 1..10) {
-                            li { +"$n" }
-                        }
-                    }
-                }
-            }
-        }
-
-        // Static feature. Try to access `/static/ktor_logo.svg`
-        static("/static") {
-            resources("static")
-        }
-
-        get("/protected/route/basic") {
-            val principal = call.principal<UserIdPrincipal>()!!
-            call.respondText("Hello ${principal.name}")
-        }
-
-        get("/protected/route/digest") {
-            val principal = call.principal<UserIdPrincipal>()!!
-            call.respondText("Hello ${principal.name}")
         }
     }
 }
